5c0e08e4f6b004e5731df7a1cc760da3465f245d
shubhssays <shubhssays@gmail.com>
2021-09-24 13:26:28 +0530

    Added Annotation

--- src/main/java/com/spts/lms/web/controllers/MessageController.java
+++ src/main/java/com/spts/lms/web/controllers/MessageController.java
@@ -11,6 +11,7 @@ import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
